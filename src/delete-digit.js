c8c97f9da8a917b7278400257f2b9fea63459b2d
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-12-20 22:50:02 +0200

    feat: add solutions for the first part of tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,11 +11,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+    function getMaxWithoutOneDigit(number, removeIdx) {
+        const arrOfDigits = [...number.toString()];
+
+        arrOfDigits.splice(removeIdx, 1);
+
+        return Number(arrOfDigits.join(''));
+    }
+
+    let max = getMaxWithoutOneDigit(n, 0);
+
+    for (let i = 1; i < n.toString().length; i += 1) {
+        const currentMax = getMaxWithoutOneDigit(n, i);
+        if (currentMax > max) max = currentMax;
+    }
+
+    return max;
 }
 
 module.exports = {
-  deleteDigit
+    deleteDigit,
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,24 +2,50 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+    const {
+        repeatTimes = 1,
+        separator = '+',
+        addition = '',
+        additionRepeatTimes = 1,
+        additionSeparator = '|',
+    } = options;
+
+    let res = '';
+    let addStr = '';
+
+    for (let i = 1; i <= additionRepeatTimes; i += 1) {
+        if (i === additionRepeatTimes) {
+            addStr += addition;
+        } else {
+            addStr += addition + additionSeparator;
+        }
+    }
+
+    for (let i = 1; i <= repeatTimes; i += 1) {
+        if (i === repeatTimes) {
+            res += str + addStr;
+        } else {
+            res += str + addStr + separator;
+        }
+    }
+
+    return res;
 }
 
 module.exports = {
-  repeater
+    repeater,
 };
